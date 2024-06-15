b9ef7cfccc6b56f967ff668b60e88a61e0cac4a9
Bank Athiwat <athiwat.sitthinanwatthana@gmail.com>
2024-06-15 23:55:34 +0700

    fix - redirect root path to products path at middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,15 @@ import { updateSession } from "./libs/lib";
 
 export async function middleware(request: NextRequest) {
     try {
+        if(request?.nextUrl.pathname === "/") {
+            return NextResponse.redirect(new URL('/products', request?.url))
+        }
         return await updateSession(request);
     } catch (error) {
         return NextResponse.redirect(new URL('/', request?.url))
     }
 }
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login|product/*).*)'],
   }
   
